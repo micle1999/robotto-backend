c83c064833387aa45b176f2d607eb9fb831ec7a6
ozgey99 <ozgey99@gmail.com>
2023-11-27 12:56:13 +0100

    Update Resource.cs

--- RobottoBackend/Models/Resource.cs
+++ RobottoBackend/Models/Resource.cs
@@ -2,6 +2,17 @@ namespace RobottoBackend.Models
 {
     public class Resource
     {
-        
+        public Guid Id { get; set; }
+        public Guid MissionId { get; set; }
+        public Guid ResourceId { get; set; }
+        public ResourceType Type { get; set; }
+        public DateTime Created { get; set; }
+        public bool Raw { get; set; }
+        public string ResourceUri { get; set; } = "";
+    }
+
+    public enum ResourceType
+    {
+        Default
     }
 }
