186597ed66c5231d76e3b716151f371439d8e4f3
ozgey99 <ozgey99@gmail.com>
2023-11-27 12:57:48 +0100

    Update Mission.cs

--- RobottoBackend/Models/Mission.cs
+++ RobottoBackend/Models/Mission.cs
@@ -2,6 +2,15 @@ namespace RobottoBackend.Models
 {
     public class Mission
     {
-        
+        public Guid Id { get; set; }
+        public string Name { get; set; } = "";
+        public MissionType Type { get; set; }
+        public DateTime Created { get; set; }
+        public string Coordinates { get; set; } = ""; // TODO: fix the type
+    }
+
+    public enum MissionType
+    {
+        Default
     }
 }
